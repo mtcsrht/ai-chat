0f56b3046a3a20b1b9a70a5414ea59fe8ec33987
Mate Cserhati <mate.cserhati03@gmail.com>
2025-06-23 09:53:09 +0200

    Initialize project structure with .sln, .csproj, and main Program.cs; add .gitignore and logging functionality.

---
+++ ai-chat/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ai_chat;
+using OpenAI.Chat;
+class Program
+{
+    private static ChatCompletion response;
+    static void Main()
+    {
+        Console.Clear();
+        var apikey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+        ChatClient client = new ChatClient(model: "gpt-4o", apiKey: apikey);
+        using StreamWriter sw = new StreamWriter("../../logs.txt",true, System.Text.Encoding.UTF8);
+        do
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("[CLIENT]: ");
+            sw.Write("[CLIENT]: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            var clientText = Console.ReadLine().Trim();
+            if (clientText != null && clientText.Length > 1)
+            {
+                sw.WriteLine(response);
+                response = client.CompleteChat(clientText);
+                
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("[ASSISTANT]: ");
+            sw.WriteLine("[ASSISTANT]");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{response.Content[0].Text}");
+            sw.WriteLine($"{response.Content[0].Text}");
+        } while (true);
+    }
+}
